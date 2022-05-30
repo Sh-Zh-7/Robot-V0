78b92372ed36169c545ea50b8b977f3097b94f45
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-31 00:59:44 +0800

    feat: add redis-like ping-pong command.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
 }
 
 group = "shzh.me"
@@ -24,6 +25,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- src/main/kotlin/shzh/me/plugins/Routing.kt
+++ src/main/kotlin/shzh/me/plugins/Routing.kt
@@ -1,19 +1,32 @@
 package shzh.me.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
+import io.ktor.server.routing.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 
 fun Application.configureRouting() {
+    routing {
+        post("/") {
+            val body = call.receiveText()
+            val bodyJson = Json.parseToJsonElement(body)
+            val postType = bodyJson.jsonObject["post_type"]!!.jsonPrimitive.content
 
-    // Starting point for a Ktor app:
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
+            when (postType) {
+                "message" -> {
+                    val msg = bodyJson.jsonObject["message"]!!.jsonPrimitive.content
+                    if (msg == "/ping") {
+                        val res = "{\"reply\": \"pong!\"}"
+                        call.respondText(res, ContentType.Application.Json, HttpStatusCode.OK)
+                    }
+                }
+                "meta_event" -> println("Heartbeat package received!")
+                else -> println("Unknown package type received!!")
+           }
         }
-    }
-    routing {
     }
 }
