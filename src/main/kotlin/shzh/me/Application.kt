01142aa75615db6264c1bcc336a06d84e3c2758e
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-05-30 11:29:18 +0800

    feat(*): project skeleton generated by ktor creator.

---
+++ src/main/kotlin/shzh/me/Application.kt
@@ -0,0 +1,11 @@
+package shzh.me
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import shzh.me.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
