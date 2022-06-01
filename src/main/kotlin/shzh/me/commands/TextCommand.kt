87a40df92d1954a6d8c55d50521e22b39a3c4eec
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-06-01 19:44:47 +0800

    feat: add upper bound for each dice.

--- src/main/kotlin/shzh/me/commands/TextCommand.kt
+++ src/main/kotlin/shzh/me/commands/TextCommand.kt
@@ -16,8 +16,8 @@ suspend fun handleDice(call: ApplicationCall, command: String, messageID: Int) {
     val (_, rule) = command.split(' ')
     val (number, count) = rule.split('d').map { str -> Integer.parseInt(str) }
 
-    if (number <= 10) {
-        val dices  = (0 until number).map { (0..count).random() }
+    if (number in 1..10 && count in 1..100) {
+        val dices  = (0 until number).map { (1..count).random() }
         val sum = dices.sum()
         val diceStr = dices.map { dice -> dice.toString() }.reduce { acc, s -> "$acc $s" }
 
