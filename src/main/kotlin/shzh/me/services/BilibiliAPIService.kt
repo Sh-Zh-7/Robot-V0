c505e1ecf6b6617c15fe017c5c8d8dd0c4b559b0
Sh-Zh-7 <zhihaoshen7@qq.com>
2022-06-03 19:07:37 +0800

    refactor: separate bilibili service file

--- src/main/kotlin/shzh/me/services/BilibiliAPIService.kt
+++ src/main/kotlin/shzh/me/services/BilibiliAPIService.kt
@@ -9,17 +9,9 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
-import org.ktorm.dsl.*
-import org.ktorm.entity.add
-import org.ktorm.entity.filter
-import org.ktorm.entity.find
-import org.ktorm.entity.toList
 import shzh.me.model.DataWrapper
 import shzh.me.model.bo.BLiveInfo
 import shzh.me.model.bo.BvData
-import shzh.me.model.dao.GroupSubBVStreamer
-import shzh.me.model.dao.db
-import shzh.me.model.dao.groupSubBVStreamers
 
 val format = Json { ignoreUnknownKeys = true }
 
@@ -81,34 +73,4 @@ suspend fun getBLiveDataByUID(userID: Long): Pair<String, String> {
     val username = bLiveJson.jsonObject["uname"]!!.jsonPrimitive.content
 
     return Pair(cover, username)
-}
-
-fun getBLiveAllSteamers(): List<GroupSubBVStreamer> {
-    return db.groupSubBVStreamers.toList()
-}
-
-fun getBLiveSteamersByGID(groupID: Long): List<GroupSubBVStreamer> {
-    return db.groupSubBVStreamers
-        .filter { it.groupID eq groupID }
-        .toList()
-}
-
-fun upsertBVStreamer(groupID: Long, userID: Long, liveID: Long) {
-    db.groupSubBVStreamers.find {
-        (it.groupID eq groupID) and (it.liveID eq liveID)
-    } ?: run {
-        val entity = GroupSubBVStreamer {
-            this.groupID = groupID
-            this.userID = userID
-            this.liveID = liveID
-        }
-        db.groupSubBVStreamers.add(entity)
-    }
-}
-
-fun deleteBVStreamer(groupID: Long, liveID: Long) {
-    val entity = db.groupSubBVStreamers.find {
-        (it.groupID eq groupID) and (it.liveID eq liveID)
-    } ?: return
-    entity.delete()
 }
---
+++ src/main/kotlin/shzh/me/services/BilibiliLiveService.kt
@@ -0,0 +1,42 @@
+package shzh.me.services
+
+import org.ktorm.dsl.and
+import org.ktorm.dsl.eq
+import org.ktorm.entity.add
+import org.ktorm.entity.filter
+import org.ktorm.entity.find
+import org.ktorm.entity.toList
+import shzh.me.model.dao.GroupSubBVStreamer
+import shzh.me.model.dao.db
+import shzh.me.model.dao.groupSubBVStreamers
+
+
+fun getBLiveAllSteamers(): List<GroupSubBVStreamer> {
+    return db.groupSubBVStreamers.toList()
+}
+
+fun getBLiveSteamersByGID(groupID: Long): List<GroupSubBVStreamer> {
+    return db.groupSubBVStreamers
+        .filter { it.groupID eq groupID }
+        .toList()
+}
+
+fun upsertBVStreamer(groupID: Long, userID: Long, liveID: Long) {
+    db.groupSubBVStreamers.find {
+        (it.groupID eq groupID) and (it.liveID eq liveID)
+    } ?: run {
+        val entity = GroupSubBVStreamer {
+            this.groupID = groupID
+            this.userID = userID
+            this.liveID = liveID
+        }
+        db.groupSubBVStreamers.add(entity)
+    }
+}
+
+fun deleteBVStreamer(groupID: Long, liveID: Long) {
+    val entity = db.groupSubBVStreamers.find {
+        (it.groupID eq groupID) and (it.liveID eq liveID)
+    } ?: return
+    entity.delete()
+}
