27b0475695d206d38f6ba32c80237c2b332b72c9
imShZH <zhihaoshen7@gmail.com>
2022-06-05 20:27:13 +0800

    feat: find history message by its content.

--- src/main/kotlin/shzh/me/commands/HistoryCommand.kt
+++ src/main/kotlin/shzh/me/commands/HistoryCommand.kt
@@ -1,6 +1,34 @@
 package shzh.me.commands
 
+import io.ktor.server.application.*
+import org.ktorm.dsl.forEachIndexed
+import shzh.me.model.dao.GroupMessages.message
+import shzh.me.model.dao.GroupMessages.username
+import shzh.me.services.getHistoryMessageCount
 import shzh.me.services.insertHistoryMessage
+import shzh.me.services.replyMessage
+import shzh.me.services.searchHistoryMessage
+import shzh.me.utils.MessageUtils
+
+suspend fun handleFindHistory(call: ApplicationCall, command: String, groupID: Long, messageID: Int) {
+    val historyCmd = command.substringAfter(' ')
+
+    val count = getHistoryMessageCount(groupID, historyCmd)
+    val query = searchHistoryMessage(groupID, historyCmd)
+
+    var result = ""
+    query.forEachIndexed{ index, row ->
+        result += "[${index + 1}] ${row[username]}: ${row[message]}\n"
+    }
+    val reply = MessageUtils
+        .builder()
+        .reply(messageID)
+        .text("共查询到${count}条数据")
+        .text(result, newline = false)
+        .text("（如超过5行数据，仅显示5行）", newline = false)
+        .content()
+    replyMessage(call, reply)
+}
 
 
 fun recordMessage(groupID: Long, userID: Long, nickname: String, message: String) {
--- src/main/kotlin/shzh/me/services/HistoryService.kt
+++ src/main/kotlin/shzh/me/services/HistoryService.kt
@@ -1,9 +1,27 @@
 package shzh.me.services
 
+import org.ktorm.dsl.*
 import org.ktorm.entity.add
 import shzh.me.model.dao.GroupMessage
+import shzh.me.model.dao.GroupMessages
 import shzh.me.model.dao.db
 import shzh.me.model.dao.groupMessages
+
+fun getHistoryMessageCount(groupID: Long, message: String): Int {
+    return db
+        .from(GroupMessages)
+        .select(count(GroupMessages.id))
+        .where { (GroupMessages.groupID eq groupID) and (GroupMessages.message like "%$message%") }
+        .map { row -> row.getInt(1) }[0]
+}
+
+fun searchHistoryMessage(groupID: Long, message: String): Query {
+    return db
+        .from(GroupMessages)
+        .select(GroupMessages.username, GroupMessages.message)
+        .where { (GroupMessages.groupID eq groupID) and (GroupMessages.message like "%$message%") }
+        .limit(5)
+}
 
 fun insertHistoryMessage(groupID: Long, userID: Long, username: String, message: String) {
     val entity = GroupMessage {
