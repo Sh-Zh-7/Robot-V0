4e8fb2165a67f1e59287aeafb2457e91b1e7e7eb
imShZH <zhihaoshen7@gmail.com>
2023-02-01 13:43:44 +0800

    refactor(*): format all sql files with IDEA built-in convention.

--- sql/group.sql
+++ sql/group.sql
@@ -1,8 +1,8 @@
 -- 历史消息
 CREATE TABLE IF NOT EXISTS group_messages (
-    id SERIAL PRIMARY KEY,
-    group_id INT8 NOT NULL,
-    user_id INT8 NOT NULL,
+    id       SERIAL PRIMARY KEY,
+    group_id INT8        NOT NULL,
+    user_id  INT8        NOT NULL,
     username VARCHAR(24) NOT NULL,
-    message TEXT NOT NULL
+    message  TEXT        NOT NULL
 );
--- sql/subscribe.sql
+++ sql/subscribe.sql
@@ -1,42 +1,47 @@
 -- B站订阅主播
-CREATE TABLE IF NOT EXISTS group_subscribed_bv_streamer(
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS group_subscribed_bv_streamer
+(
+    id       SERIAL PRIMARY KEY,
     group_id INT8 NOT NULL,
-    user_id INT8 NOT NULL,
-    live_id INT8 NOT NULL
+    user_id  INT8 NOT NULL,
+    live_id  INT8 NOT NULL
 );
 
 -- B站用户动态
-CREATE TABLE IF NOT EXISTS group_subscribed_bv_user(
-    id SERIAL PRIMARY KEY,
-    group_id INT8 NOT NULL,
-    user_id INT8 NOT NULL,
+CREATE TABLE IF NOT EXISTS group_subscribed_bv_user
+(
+    id        SERIAL PRIMARY KEY,
+    group_id  INT8 NOT NULL,
+    user_id   INT8 NOT NULL,
     published INT8 NOT NULL
 );
 
 -- Github订阅用户
-CREATE TABLE IF NOT EXISTS group_subscribed_github(
-    id SERIAL PRIMARY KEY,
-    group_id INT8 NOT NULL,
-    username VARCHAR(40) NOT NULL,
+CREATE TABLE IF NOT EXISTS group_subscribed_github
+(
+    id        SERIAL PRIMARY KEY,
+    group_id  INT8        NOT NULL,
+    username  VARCHAR(40) NOT NULL,
     published TIMESTAMP
 );
 
 -- 微博订阅用户
-CREATE TABLE IF NOT EXISTS group_subscribed_weibo(
-    id SERIAL PRIMARY KEY,
-    group_id INT8 NOT NULL,
-    weibo_id INT8 NOT NULL,
-    username VARCHAR(32) NOT NULL,
+CREATE TABLE IF NOT EXISTS group_subscribed_weibo
+(
+    id        SERIAL PRIMARY KEY,
+    group_id  INT8        NOT NULL,
+    weibo_id  INT8        NOT NULL,
+    username  VARCHAR(32) NOT NULL,
     published TIMESTAMP
 );
 
 -- 知乎订阅用户
-CREATE TABLE IF NOT EXISTS group_subscribed_zhihu(
-    id SERIAL PRIMARY KEY,
-    group_id INT8 NOT NULL,
+CREATE TABLE IF NOT EXISTS group_subscribed_zhihu
+(
+    id       SERIAL PRIMARY KEY,
+    group_id INT8        NOT NULL,
     username VARCHAR(64) NOT NULL,
-    answer TIMESTAMP,
-    post TIMESTAMP,
-    pin TIMESTAMP
+    answer   TIMESTAMP,
+    post     TIMESTAMP,
+    pin      TIMESTAMP
 );
